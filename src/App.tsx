37e24303a0773b7adbbc44d4db594a1a27d2c2c3
Arvin Buid <arvinbuid01@gmail.com>
2025-04-25 18:45:09 +0800

    Created Hero component and reusable Container component, build Navbar component logo and navlinks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Layout from "./components/Layout"
+import Hero from "./components/sections/Hero"
 
 const Home = () => {
   return (
     <Layout title="Modern Landing Page">
-      <h1>Main content</h1>
+      <Hero />
     </Layout>
   )
 }
